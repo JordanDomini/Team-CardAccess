5d7a79b983cc063ee6da557ca9648c9dc0d0c8e6
JordanDomini <31752057+JordanDomini@users.noreply.github.com>
2019-11-11 15:13:47 -0800

    Update TriggerTest.py

--- src/TriggerTest.py
+++ src/TriggerTest.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO           # import RPi.GPIO module
 import keyboard                   # using module keyboard
 import time
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD
 GPIO.setup(24, GPIO.OUT)           # set a port/pin as an output
-GPIO.setwarnings(False)
 i = 0
 time.sleep(1)
 while True:                       # making a loop
