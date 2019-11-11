cbe35ab18698764ac9cfbcc75d109cef1d029f46
JordanDomini <31752057+JordanDomini@users.noreply.github.com>
2019-11-11 15:01:35 -0800

    Update TriggerTest.py

--- src/TriggerTest.py
+++ src/TriggerTest.py
@@ -2,11 +2,12 @@ import RPi.GPIO as GPIO           # import RPi.GPIO module
 import keyboard                   # using module keyboard
 GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD
 GPIO.setup(7, GPIO.OUT)           # set a port/pin as an output
+GPIO.setwarnings(FALSE)
 i = 0
 
 while True:                       # making a loop
     try:                          # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed(' '):  # if key 'q' is pressed
+        if keyboard.read_key():  # if key 'q' is pressed
             if i == 0:
                 GPIO.output(7, 1)       # set port/pin value to 1/GPIO.HIGH/True
                 i = 1
