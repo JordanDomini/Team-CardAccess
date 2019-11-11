5e3a0d9467f5767137a54a2f890672ce41251ffd
JordanDomini <31752057+JordanDomini@users.noreply.github.com>
2019-11-11 15:42:47 -0800

    Update TriggerTest.py

--- src/TriggerTest.py
+++ src/TriggerTest.py
@@ -1,23 +1,24 @@
 import RPi.GPIO as GPIO           # import RPi.GPIO module
 import keyboard                   # using module keyboard
 import time
-GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD
 GPIO.setup(24, GPIO.OUT)           # set a port/pin as an output
 i = 0
 time.sleep(1)
-while True:                       # making a loop                         # used try so that if user pressed other than the given key error will not be shown
-    if keyboard.is_pressed('o'):  # if o is pressed
-        if i == 0:
-
-            GPIO.output(24, 1)       # set port/pin value to 1/GPIO.HIGH/True
-            i = 1
-            time.sleep(1)
-            break             # finishing the loop
+try:
+    while True:                       # making a loop
+        if keyboard.is_pressed('o'):  # if o is pressed
+            if i == 0:
+                GPIO.output(24, 1)       # set port/pin value to 1/GPIO.HIGH/True
+                i = 1
+                time.sleep(1)
+                break             # finishing the loop
+            else:
+                GPIO.output(24, 0)  # set port/pin value to 0/GPIO.LOW/False
+                i = 0
+                time.sleep(1)
+                break             # finish the loop
         else:
-            GPIO.output(24, 0)  # set port/pin value to 0/GPIO.LOW/False
-            i = 0
-            time.sleep(1)
-            break             # finish the loop
-    else:
-        pass
+            pass
+except:
+    GPIO.cleanup()
