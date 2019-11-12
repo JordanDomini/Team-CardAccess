c7313f4960a273440a4ae141a24a24cea19836f9
JordanDomini <31752057+JordanDomini@users.noreply.github.com>
2019-11-12 15:04:57 -0800

    Added test for card reader and full prototype

--- src/PrototypeTest.py
+++ src/PrototypeTest.py
@@ -2,23 +2,34 @@
 import RPi.GPIO as GPIO           # import RPi.GPIO module
 import keyboard                   # using module keyboard
 import time
+from mfrc522 import SimpleMFRC522
 
 GPIO.setmode(GPIO.BOARD)            # choose BCM or BOARD
 GPIO.setup(26, GPIO.OUT)           # set a port/pin as an output
+GPIO.setup(29, GPIO.OUT)
+
+reader = SimpleMFRC522()
+
 i = 0
 time.sleep(1)
+
 try:
     while True:                       # making a loop
-        if keyboard.is_pressed('o'):  # if o is pressed
+        id, text = reader.read()      # waiting for id to be scanned
+        if id.contains("629587"):  # if id is correct
             if i == 0:
                 print("\nTurning on.")
                 GPIO.output(26, 1)       # set port/pin value to 1/GPIO.HIGH/True
+                GPIO.output(29, 1)
                 i = 1
                 time.sleep(1)
             elif i == 1:
                 print("\nTurning off.")
                 GPIO.output(26, 0)  # set port/pin value to 0/GPIO.LOW/False
+                GPIO.output(29, 0)
                 i = 0
                 time.sleep(1)
+        else:
+
 except:
     GPIO.cleanup()
