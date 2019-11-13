4c315a8a8d8398f7319d32edfcd2cf4750d088ea
JordanDomini <31752057+JordanDomini@users.noreply.github.com>
2019-11-13 14:43:33 -0800

    Create ImprovedPrototypeTest.py
    Added Id checking and a buffer to prevent repetetive scanning

---
+++ src/ImprovedPrototypeTest.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO           # import RPi.GPIO module
+import time
+from mfrc522 import SimpleMFRC522
+
+GPIO.setmode(GPIO.BOARD)            # choose BCM or BOARD
+GPIO.setup(26, GPIO.OUT)           # set a port/pin as an output
+GPIO.setup(29, GPIO.OUT)
+GPIO.setup(31, GPIO.OUT)
+
+reader = SimpleMFRC522()
+lastId = 0
+i = 0
+future = time.time() + 2
+time.sleep(2)
+
+try:
+    while True:                       # making an infinite loop
+        id, text = reader.read()      # waiting for id to be scanned
+        while time.time() < future:
+            if reader.read():
+                future = time.time() + 2
+        if id == 470495939090:        # if id is correct
+            if i == 0:                # check if the system is off
+                print("\nTurning on.")
+                GPIO.output(26, 1)       # set port/pin value to 1/GPIO.HIGH/True
+                GPIO.output(29, 1)
+                i = 1
+            elif i == 1:             # check if the system is on
+                print("\nTurning off.")
+                GPIO.output(26, 0)  # set port/pin value to 0/GPIO.LOW/False
+                GPIO.output(29, 0)
+                i = 0
+            else:
+                GPIO.output(31, 0)
+
+
+except:
+    GPIO.cleanup()
