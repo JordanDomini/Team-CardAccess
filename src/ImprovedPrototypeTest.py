5de0a9a6f4abc5752fdecdad88a3dd88705ac513
JordanDomini <31752057+JordanDomini@users.noreply.github.com>
2019-11-13 16:07:15 -0800

    Update ImprovedPrototypeTest.py

--- src/ImprovedPrototypeTest.py
+++ src/ImprovedPrototypeTest.py
@@ -11,30 +11,28 @@ GPIO.setup(31, GPIO.OUT)
 reader = SimpleMFRC522()
 i = 0
 GPIO.output(26, 1)
-future = time.monotonic() + 4
-time.sleep(2)
+future = time.monotonic() + 1
 
 try:
     while True:                       # making an infinite loop
         id, text = reader.read()      # waiting for id to be scanned
         while time.monotonic() < future:
             if reader.read():
-                future = time.monotonic() + 2
-                time.sleep(1)
+                future = time.monotonic() + 1
         if '470495939090' in str(id):        # if id is correct
             if i == 0:                # check if the system is off
                 print("\nTurning on.")
-                GPIO.output(26, 0)       # set port/pin value to 1/GPIO.HIGH/True
+                GPIO.output(26, 0)
                 GPIO.output(29, 0)
                 i = 1
             elif i == 1:             # check if the system is on
                 print("\nTurning off.")
-                GPIO.output(26, 1)  # set port/pin value to 0/GPIO.LOW/False
+                GPIO.output(26, 1)
                 GPIO.output(29, 1)
                 i = 0
             else:
                 GPIO.output(31, 1)
         time.sleep(1)
-        future = time.monotonic() + 2
+        future = time.monotonic() + 1
 except:
     GPIO.cleanup()
