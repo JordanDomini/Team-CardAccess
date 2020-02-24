ffdc9c3d836bad0ff419db8d1098e5517965c564
JordanDomini <31752057+JordanDomini@users.noreply.github.com>
2020-02-24 14:57:02 -0800

    Update DataFunctions.py

--- src/DataFunctions.py
+++ src/DataFunctions.py
@@ -84,8 +84,14 @@ def check_user_permission(scanned_tag):
 # checks the user type
 def check_user_level(scanned_tag):
     session = Session()
-    req_user = session.query(User).filter_by(rfid_tag=scanned_tag).first
-    return req_user.Type
+    req_user = session.query(User.User).filter_by(rfid_tag=scanned_tag).first
+    if req_user:
+        if req_user.Type == 1:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 
 # gets user
