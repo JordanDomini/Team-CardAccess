5c53d63a71686f077755263b1a50f8123feb906f
JordanDomini <31752057+JordanDomini@users.noreply.github.com>
2020-02-28 00:23:04 -0800

    Update Main Screen.py

--- src/Main Screen.py
+++ src/Main Screen.py
@@ -599,7 +599,7 @@ def edit_user():
 def main_screen():
     global screen
     screen = Tk()
-    screen.geometry("300x250+1110+665")
+    screen.geometry("300x250+810+415")
     screen.title("Main Screen")
     Label(text="Main Screen", bg="grey", width="300", height="2", font=("Calibri", 13)).pack()
     Label(text="").pack()
